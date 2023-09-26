b26b1dc3d64291288e813b7150277c95efcb4cb4
Mariel de la Garza <mariel.delagarza@viget.com>
2023-09-26 14:39:38 -0400

    Initial data setup - from testing repo
    Copy over work from a dummy/testing repo on @mariel-delagarza's
    account. First work-through of connecting data to table and updating
    table to handle it.

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,62 +1,68 @@
 <script>
-  import { onMount } from "svelte"
-  import tooltip from "../js/tooltip"
-  import Icon from "./Icons.svelte"
+  import { onMount } from "svelte";
+  import tooltip from "../js/tooltip";
+  import Icon from "./Icons.svelte";
 
   export let filteredData;
   export let row;
 
-  let sortIconContainer
-  $: sortClass = "inactive"
+  let sortIconContainer;
+  $: sortClass = "inactive";
 
-  const sortByColumns = ['activity', 'state', 'authority', 'type of resource']
+  const sortByColumns = [
+    "date",
+    "activity",
+    "state",
+    "authority",
+    "type of resource",
+  ];
 
   function handleClick(e) {
-    let title = undefined
-    let currentRow = undefined
-    let extraContent = undefined
-    
-    if (e.target.parentNode.classList.contains('title') ) {
-      title = e.target.parentNode
-      currentRow = title.nextElementSibling
+    let title = undefined;
+    let currentRow = undefined;
+    let extraContent = undefined;
+
+    if (e.target.parentNode.classList.contains("title")) {
+      title = e.target.parentNode;
+      currentRow = title.nextElementSibling;
       extraContent = e.target.parentNode.nextElementSibling;
     } else {
-      title = e.target.parentNode.parentNode
-      currentRow = title.nextElementSibling
+      title = e.target.parentNode.parentNode;
+      currentRow = title.nextElementSibling;
       extraContent = e.target.parentNode.parentNode.nextElementSibling;
     }
 
-    title.classList.toggle('title--active')
-    title.classList.toggle('table__body__cell--border')
-    currentRow.classList.toggle('table__body__cell--border')
+    title.classList.toggle("title--active");
+    title.classList.toggle("table__body__cell--border");
+    currentRow.classList.toggle("table__body__cell--border");
     // Show/Hide extraContent
-    extraContent.classList.toggle('active');
+    extraContent.classList.toggle("active");
     extraContent.classList.toggle("hide");
-    (row.isOpen) ? row.isOpen = true : row.isOpen = !row.isOpen
+    row.isOpen ? (row.isOpen = true) : (row.isOpen = !row.isOpen);
   }
 
   const headerNames = [
     "Activity",
+    "Date",
     "State",
-    "Policy Goals",
     "Authority",
     "Type of Resource",
     "Tags",
   ];
 
   $: sortBy = { col: "activity", ascending: true };
-  
+
   $: sort = (e, column) => {
     column = column.toLowerCase().replace(/\s/g, "_"); // replace spaces using regex with undesrscore
-    const iconsActive = document.querySelectorAll('.sort-icon--active');
-    iconsActive.forEach(icon => {
-      icon.classList.remove('sort-icon--active');
+    const iconsActive = document.querySelectorAll(".sort-icon--active");
+    iconsActive.forEach((icon) => {
+      icon.classList.remove("sort-icon--active");
     });
     if (sortBy.col == column) {
       sortBy.ascending = !sortBy.ascending;
-      sortClass = sortBy.ascending ? 'active' : 'inactive';
+      sortClass = sortBy.ascending ? "active" : "inactive";
     } else {
-      sortClass = 'inactive';
+      sortClass = "inactive";
       sortBy.col = column;
       sortBy.ascending = true;
     }
@@ -64,36 +70,59 @@
     // Modifier to sorting function for ascending or descending
     let sortModifier = sortBy.ascending ? 1 : -1;
 
-    // Sort by activity title
-    if (column == "activity") {
-      return (filteredData = filteredData.sort((a, b) => {
-        if (a.activity.title < b.activity.title) {
-          return -1 * sortModifier;
-        } else if (a.activity.title > b.activity.title) {
-          return 1 * sortModifier;
-        } else {
-          return 0;
-        }
-      }));
-    }
+    // current function - does NOT also sort by date,
+    // only puts event titles in alpha order
+    let sortActivity = (a, b) =>
+      a.activity.title < b.activity.title
+        ? -1 * sortModifier
+        : a.activity.title > b.activity.title
+        ? 1 * sortModifier
+        : 0;
 
-    let sort = (a, b) =>
+    // possible option - will sort by date, and then
+    // within that date, sort events by alpha order
+    // so dates within 01/01/22 - 01/31/22 are in alpha order,
+    // then dates 02/01/22 - 02/28/22 are in alpha order.
+    // not called right now -- user probably expects only alpha sort
+    let sortByNameThenDate = (a, b) => {
+      let rv = a.date - b.date;
+      if (rv === 0) {
+        rv = a.activity.title.localeCompare(b.activity.title);
+      }
+      return rv;
+    };
+
+    let sortDate = (a, b) =>
+      a.date < b.date
+        ? -1 * sortModifier
+        : a.date > b.date
+        ? 1 * sortModifier
+        : 0;
+
+    let sortColumnName = (a, b) =>
       a[column] < b[column]
         ? -1 * sortModifier
         : a[column] > b[column]
         ? 1 * sortModifier
         : 0;
 
-    filteredData = filteredData.sort(sort);
+    //Sort by activity title
+    if (column == "activity") {
+      return (filteredData = filteredData.sort(sortActivity));
+    }
+
+    filteredData = filteredData.sort(sortDate).sort(sortColumnName);
   };
 
   onMount(() => {
-    const iconsActive = document.querySelectorAll('.sort-icon--active');
-    iconsActive.forEach(icon => {
-      icon.classList.remove('sort-icon--active');
+    const iconsActive = document.querySelectorAll(".sort-icon--active");
+    iconsActive.forEach((icon) => {
+      icon.classList.remove("sort-icon--active");
     });
-    const divActivity = document.querySelector('.table__cell--header__container__activity');
-    divActivity.children[1].children[1].classList.add('sort-icon--active');
+    const divActivity = document.querySelector(
+      ".table__cell--header__container__activity"
+    );
+    divActivity.children[1].children[1].classList.add("sort-icon--active");
     // Sync horizontal scroll of table header and table body
     // Inspired by https://codepen.io/Goweb/pen/rgrjWx
     const scrollSync = () => {
@@ -112,7 +141,6 @@
       bindSyncScrolling(tableHeader, tableBody);
     };
     scrollSync();
-
   });
 </script>
 
@@ -123,13 +151,37 @@
         <tr class="table__header-row">
           {#each headerNames as name}
             <th class="table__cell--header" scope="col">
-              <div class="table__cell--header__container table__cell--header__container__{name.toLowerCase().split(' ').join('-')}" on:click={ (sortByColumns.includes(name.toLowerCase())) ? (e) => sort(e, name) : ''}>
+              <!-- svelte-ignore a11y-click-events-have-key-events -->
+              <div
+                class="table__cell--header__container table__cell--header__container__{name
+                  .toLowerCase()
+                  .split(' ')
+                  .join('-')}"
+                on:click={sortByColumns.includes(name.toLowerCase())
+                  ? (e) => sort(e, name)
+                  : ""}
+              >
                 <span>{name}</span>
                 {#if sortByColumns.includes(name.toLowerCase())}
-                <div class="sort-icons-container" on:click={(e) => sort(e, name)}>
-                  <button class="sort-icon sort-icon--{(sortBy.col == name.toLowerCase().split(' ').join('_') && sortBy.ascending ) ? 'inactive' : 'active'}">▲</button>
-                  <button class= "sort-icon sort-icon--{(sortBy.col == name.toLowerCase().split(' ').join('_') && sortBy.ascending ) ? 'active' : 'inactive'}">▼</button>
-                </div>
+                  <div
+                    class="sort-icons-container"
+                    on:click={(e) => sort(e, name)}
+                  >
+                    <button
+                      class="sort-icon sort-icon--{sortBy.col ==
+                        name.toLowerCase().split(' ').join('_') &&
+                      sortBy.ascending
+                        ? 'inactive'
+                        : 'active'}">▲</button
+                    >
+                    <button
+                      class="sort-icon sort-icon--{sortBy.col ==
+                        name.toLowerCase().split(' ').join('_') &&
+                      sortBy.ascending
+                        ? 'active'
+                        : 'inactive'}">▼</button
+                    >
+                  </div>
                 {/if}
               </div>
             </th>
@@ -142,24 +194,47 @@
     <table class="table table__body">
       <tbody>
         {#each filteredData as rows}
-          <tr on:click={(e) => handleClick(e)} class="title table__body__cell--border">
-            <td class="table__body__cell table__body__cell--data"><div class="table__body__cell__title-container"><span class="icon-container"></span>{rows.activity.title}</div></td>
-            <td class="table__body__cell table__body__cell--data">{rows.state}</td>
-            <td class="table__body__cell table__body__cell--data">
-              <div class="table__body__cell__policy-goal-container">
-                {#each rows.policy_goals as policyGoal}
-                  <span class="table__body__cell__policy-goal table__body__cell__policy-goal--{policyGoal.toLowerCase()}">{policyGoal.split('_').join(' ')}</span>
-                {/each}
-              </div>
-            </td>
-            <td class="table__body__cell table__body__cell--data">{rows.authority}</td>
-            <td class="table__body__cell table__body__cell--data">{rows.type_of_resource}</td>
-            <td class="table__body__cell table__body__cell--data table__body__cell__icon-container">
+          <tr
+            on:click={(e) => handleClick(e)}
+            class="title table__body__cell--border"
+          >
+            <!-- event name/title -->
+            <td class="table__body__cell table__body__cell--data"
+              ><div class="table__body__cell__title-container">
+                <span class="icon-container" />{rows.activity.title}
+              </div></td
+            >
+            <!-- event date - displays string value -->
+            <td class="table__body__cell table__body__cell--data"
+              >{rows.date_string}</td
+            >
+            <!-- event category -->
+            <td class="table__body__cell table__body__cell--data"
+              >{rows.state}</td
+            >
+            <!-- event speaker -->
+            <td class="table__body__cell table__body__cell--data"
+              >{rows.authority}</td
+            >
+            <!-- event type -->
+            <td class="table__body__cell table__body__cell--data"
+              >{rows.type_of_resource}</td
+            >
+            <td
+              class="table__body__cell table__body__cell--data table__body__cell__icon-container"
+            >
               {#each rows.tags as tag}
-                <span class="icon-tag-container" use:tooltip={{theme: 'energy'}} aria-hidden="true" aria-label={tag}><Icon name="icon {tag}" class="icon__tags"/></span>
+                <span
+                  class="icon-tag-container"
+                  use:tooltip={{ theme: "energy" }}
+                  aria-hidden="true"
+                  aria-label={tag}
+                  ><Icon name="icon {tag}" class="icon__tags" /></span
+                >
               {/each}
             </td>
           </tr>
+          <!--this tr is the stuff under the dropdown -->
           <tr class="extra-content hide">
             <td class="table__body__cell" colspan="6">
               <div class="extra-content__container">
@@ -168,16 +243,23 @@
                   <a
                     href={rows.activity.link}
                     target="_blank"
-                    rel="noopener noreferrer">Go to resource<span class="icon-container"><Icon name="Icon-open-blank" class="icon"/></span></a
+                    rel="noopener noreferrer"
+                    >Go to resource<span class="icon-container"
+                      ><Icon name="Icon-open-blank" class="icon" /></span
+                    ></a
                   >
                 </div>
               </div>
             </td>
           </tr>
         {:else}
+          <!-- if filters return an empty table -->
           <tr>
-            <td class="table__body__cell table__body__cell--no-data"  colspan="6"><p class="table__body__cell__no-data__title">0 entries found.</p>
-            <p class="table__body__cell__no-data__desc">Try changing or removing filters to adjust the results.</p>
+            <td class="table__body__cell table__body__cell--no-data" colspan="6"
+              ><p class="table__body__cell__no-data__title">0 entries found.</p>
+              <p class="table__body__cell__no-data__desc">
+                Try changing or removing filters to adjust the results.
+              </p>
             </td>
           </tr>
         {/each}
@@ -187,22 +269,27 @@
 </div>
 
 <style lang="scss">
-  @use '../scss/abstracts/' as *;
+  @use "../scss/abstracts/" as *;
   @use "../scss/components/table";
-  :global(.tippy-box[data-theme~='energy']) {
+  :global(.tippy-box[data-theme~="energy"]) {
     @extend %text-style-ui-4;
     color: $color-text-gray-500;
     background-color: $color-background-white;
     padding: rem(6) rem(6) rem(8) rem(6);
-    filter: drop-shadow(0px 1px 9px rgba(0, 0, 0, 0.06)) drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.1));
+    filter: drop-shadow(0px 1px 9px rgba(0, 0, 0, 0.06))
+      drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.1));
   }
 
   :global(
-    .tippy-box[data-theme~='energy'][data-placement^='top'] > .tippy-arrow::before,
-    .tippy-box[data-theme~='energy'][data-placement^='bottom'] > .tippy-arrow::before,
-    .tippy-box[data-theme~='energy'][data-placement^='left'] > .tippy-arrow::before,
-    .tippy-box[data-theme~='energy'][data-placement^='right'] > .tippy-arrow::before
-  ){
+      .tippy-box[data-theme~="energy"][data-placement^="top"]
+        > .tippy-arrow::before,
+      .tippy-box[data-theme~="energy"][data-placement^="bottom"]
+        > .tippy-arrow::before,
+      .tippy-box[data-theme~="energy"][data-placement^="left"]
+        > .tippy-arrow::before,
+      .tippy-box[data-theme~="energy"][data-placement^="right"]
+        > .tippy-arrow::before
+    ) {
     border-top-color: $color-background-white;
   }
 </style>
--- src/data.js
+++ src/data.js
@@ -1,62 +1,141 @@
-import * as d3Fetch from 'd3-fetch'
+import * as d3Fetch from "d3-fetch";
 
-const URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZbsWyNWxgbsJJd2AuaoNIJ2KkEplWSNK77gxcS_WndRrj1rNnPoxtPNl60HjlmdvQo4UvxBUMEi1S/pub?output=csv";
+const URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRq1A_i1wvnY0cPdInPbf50cgeiJOPF-B_iEROEzpvrMYM7O_k_Ee3qy8lhLitG60Q_oy2G6961yd_G/pub?output=csv";
 
-const policyGoals = ['Emissions_Reduction', 'Economic_Development', 'Resilience']
-const tags = ['Anticipating_Climate_Impacts', 'Comprehensive_Planning_Grid_Modernization', 'Data_Transparency_or_Visualization', 'Electric_Vehicles', 'Energy_Storage', 'Environmental_Justice', 'Innovation_and_Clusters', 'Local-Level_Planning_or_Support', 'Microgrids', 'Distributed_Energy_Resources_(DERs)', 'Technology_or_System_Standards', 'Workforce_Development']
+const tags = [
+  "Anticipating_Climate_Impacts",
+  "Comprehensive_Planning_Grid_Modernization",
+  "Data_Transparency_or_Visualization",
+  "Electric_Vehicles",
+  "Energy_Storage",
+  "Environmental_Justice",
+  "Innovation_and_Clusters",
+  "Local-Level_Planning_or_Support",
+  "Microgrids",
+  "Distributed_Energy_Resources_(DERs)",
+  "Technology_or_System_Standards",
+  "Workforce_Development",
+];
 
 export default function getData() {
-  const dataPromise = d3Fetch.csv(URL).then(res => {
-
+  const dataPromise = d3Fetch.csv(URL).then((res) => {
+    //console.log(res);
     const data = res.map((row, index) => {
       return {
         id: index,
-        policy_goals: policyGoals.filter((goalName) => row[goalName]),
-        tags: tags.filter((tagName) => row[tagName]).map(tagName => tagName.split('_').join(' ')),
+        tags: tags
+          .filter((tagName) => row[tagName])
+          .map((tagName) => tagName.split("_").join(" ")),
         activity: {
+          //poni: title
           title: row.title,
-          description: row.description,
-          link: row.URL
+          //poni: quote
+          description: row.quote,
+          //poni: source_1
+          link: row.source_1,
+          vpn_required_1: row.vpn_required_1,
+          source_2: row.source_2,
+          vpn_required_2: row.vpn_required_2,
+          source_3: row.source_3,
+          vpn_required_3: row.vpn_required_3,
+          image_url: row.image_url,
+          image_source: row.image_source,
+          key_moment: row.key_moment,
         },
-        state: row.state,
-        state_name: row.state_name,
-        authority: row.authority,
-        type_of_resource: row.type_of_resource,
-      }
-    })
+        //poni: category
+        state: row.category,
+        //poni: category
+        state_name: row.category_name,
+        //poni: speaker
+        authority: row.speaker,
+        authority_name: "",
+        //poni: type
+        type_of_resource: row.type,
+        //poni: date,
+        date_string: row.date,
+        date: "",
+      };
+    });
 
-    const authority = formatAuthority(data)
+    console.log("Data", data);
+    const authority = formatAuthority(data);
 
-    const resourceTypes = formatResourceType(data)
+    const authority_name = createAndAssignAuthorityNames(data);
 
-    const states = formatStates(data)
+    const resourceTypes = formatResourceType(data);
 
+    const states = formatStates(data);
+
+    const dates = createAndAssignDateObjects(data);
     return {
       data: data,
+      //poni: category
       states: states,
-      tags: tags.map(tagName => tagName.split('_').join(' ')),
+      dates: dates,
+      tags: tags.map((tagName) => tagName.split("_").join(" ")),
+      //poni: speaker
       authority: authority,
+      //speaker without title - for dropdown
+      authority_name: authority_name,
+      //poni: type
       resourceTypes: resourceTypes,
-      policyGoals: policyGoals,
+    };
+  });
+  return dataPromise;
+}
+
+function createAndAssignAuthorityNames(array) {
+  let authority_Name_Array = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let name = array[i].authority.split(",")[0];
+    if (array[i].authority != "") {
+      array[i].authority_name = name;
+
+      if (!authority_Name_Array.includes(name)) {
+        authority_Name_Array.push(name);
+      }
     }
-  })
-  return dataPromise
+  }
+
+  return authority_Name_Array;
+}
+
+function createAndAssignDateObjects(array) {
+  let dates = [];
+  let date_string = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let date = array[i].date_string;
+
+    let dateObject = new Date(date);
+    array[i].date = dateObject;
+
+    if (!date_string.includes(array[i].date_string)) {
+      date_string.push(array[i].date_string);
+      dates.push(dateObject);
+    }
+  }
+
+  return dates;
 }
 
 function formatAuthority(array) {
-  return [...new Set(array.map(el => el.authority))]
+  return [...new Set(array.map((el) => el.authority))];
 }
 
 function formatResourceType(array) {
-  return [...new Set(array.map(el => el.type_of_resource))]
+  return [...new Set(array.map((el) => el.type_of_resource))];
 }
 
-function formatStates(row) {
-  return [...new Set(row.map(r => r.state))]
-    .map(state => {
-      return {
-        name: row.find(r => r.state === state).state_name,
-        value: row.find(r => r.state === state).state,
-      }
-    })
+function formatStates(array) {
+  // return [...new Set(row.map((r) => r.state))].map((state) => {
+  //   return {
+  //     name: row.find((r) => r.state === state).state_name,
+  //     value: row.find((r) => r.state === state).state,
+  //   };
+  // });
+
+  return [...new Set(array.map((el) => el.state))];
 }
