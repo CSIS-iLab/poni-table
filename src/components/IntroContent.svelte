0faf2b8c05aa8038379738be5bcf99305f375964
Mariel de la Garza <mariel.delagarza@viget.com>
2023-10-05 11:58:28 -0400

    Update to match main

--- src/components/IntroContent.svelte
+++ src/components/IntroContent.svelte
@@ -4,31 +4,26 @@
 
 <div class="wrapper">
   <main class="container intro-content">
-    <div class="intro-content__container">
-      <div class="intro-content__flex-left">
-        <p class="intro-content__overline--regular">
-          By the Project on Nuclear Issues
-        </p>
-        <h1 class="intro-content__title">Russian Nuclear Threats Timeline</h1>
-        <p class="intro-content__introduction">
-          A curated database of events illustrating the evolution of nuclear
-          threats in the leadup to &mdash; and throughout the first year of
-          &mdash; the War in Ukraine. From the <a
-            class="intro-content__link"
-            href="https://www.csis.org/programs/international-security-program/project-nuclear-issues"
-            target="_blank">CSIS Project on Nuclear Issues</a
-          >.
-        </p>
-        <p class="intro-content__more">
-          <a class="intro-content__link intro-content__link--more" href="#about"
-            ><span class="intro-content__more__icon-container">
-              <Icon class="icon__info" name="Icon-info" />
-            </span><span>More on this database</span></a
-          >
-        </p>
-      </div>
-      <div class="intro-content__flex-right"></div>
-    </div>
+    <p class="intro-content__overline--regular">
+      By the Project on Nuclear Issues
+    </p>
+    <h1 class="intro-content__title">Russian Nuclear Threats Timeline</h1>
+    <p class="intro-content__introduction">
+      A curated database of events illustrating the evolution of nuclear threats
+      in the leadup to - and throughout the first year of - the War in Ukraine.
+      From the <a
+        class="intro-content__link"
+        href="https://www.csis.org/programs/international-security-program/project-nuclear-issues"
+        target="_blank">CSIS Project on Nuclear Issues</a
+      >.
+    </p>
+    <p class="intro-content__more">
+      <a class="intro-content__link intro-content__link--more" href="#about"
+        ><span class="intro-content__more__icon-container">
+          <Icon class="icon__info" name="Icon-info" />
+        </span><span>More on this database</span></a
+      >
+    </p>
   </main>
 </div>
 
