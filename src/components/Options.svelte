98756311e73d40b80c0aacdc0f40d38498ad89c7
Mariel de la Garza <mariel.delagarza@viget.com>
2023-10-20 14:41:23 -0400

    Update placeholder text color, dropdown text color

--- src/components/Options.svelte
+++ src/components/Options.svelte
@@ -389,6 +389,7 @@
   :global(.selectContainer) {
     --internalPadding: 0 4px 0 0 !important;
     --itemFirstBorderRadius: 2px;
+    --placeholderColor: $color-placeholder;
   }
 
   :global(.selectContainer .item) {
